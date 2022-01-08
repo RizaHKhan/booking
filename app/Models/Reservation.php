cafd5d4af5e6a3b422cc2028e936a92a1b323d02
Riza Khan <rizakhan@Rizas-Mac-mini.local>
2022-01-07 23:36:39 -0500

    Updating relationship to use belongsTo

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -4,16 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Reservation extends Model
 {
     use HasFactory;
 
-    public function user(): HasOne
+    public function user(): BelongsTo
     {
-        return $this->hasOne(User::class, 'id', 'user_id');
+        return $this->belongsTo(User::class);
     }
 
     public function rooms(): BelongsToMany
